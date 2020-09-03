a2cd9436377c912f5ccc35a1f63c19dbd2cebf1f
anikchowdhury440 <anikchowdhury440@gmail.com>
2020-09-03 17:40:12 +0530

    Store Daily Wages along with total wages

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -26,13 +26,20 @@ function getWorkingHours(){
 	echo $empHrs
 }
 
+function calcDailyWage() {
+	local workHrs=$1
+	wage=$(( $workHrs*$EMP_RATE_PER_HR ))
+	echo $wage
+}
+
 while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
 	((totalWorkingDays++))
 	workHours="$( getWorkingHours $((RANDOM%3)) )"
-	empCheck=$((RANDOM%3))
 	totalEmpHrs=$(($totalEmpHrs+$workHours))
+	empDailyWage[$totalWorkingDays]="$( calcDailyWage $workHours )"
 done
 
-totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR))
+totalSalary="$( calcDailyWage $totalEmpHrs )"
+echo "Daily wage " ${empDailyWage[@]}
 
