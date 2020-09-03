6c431a59996c60e763db0d81de00addb405f6551
anikchowdhury440 <anikchowdhury440@gmail.com>
2020-09-03 17:16:37 +0530

    Employee function to get work hours code refactoring

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -11,22 +11,28 @@ NUM_WORKING_DAYS=20
 totalEmpHrs=0
 totalWorkingDays=0
 
+function getWorkingHours(){
+	case $1 in
+                $IS_FULL_TIME)
+                        empHrs=8
+                        ;;
+                $IS_PART_TIME)
+                        empHrs=4
+                        ;;
+                *)
+                        empHrs=0
+                        ;;
+        esac
+	echo $empHrs
+}
+
 while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
 	((totalWorkingDays++))
+	workHours="$( getWorkingHours $((RANDOM%3)) )"
 	empCheck=$((RANDOM%3))
-	case $empCheck in
-		$IS_FULL_TIME)
-			empHrs=8
-			;;
-		$IS_PART_TIME)
-			empHrs=4
-			;;
-		*)
-			empHrs=0
-			;;
-	esac
-	totalEmpHrs=$(($totalEmpHrs+$empHrs))
+	totalEmpHrs=$(($totalEmpHrs+$workHours))
 done
 
 totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR))
+
